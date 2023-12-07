f987dbc580e0f42ec6eb14883b63c18680c7b347
t-sasaki915 <netst915@gmail.com>
2023-12-07 11:36:03 +0900

    Dump specified file

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main (main) where
 
-import Lib
+import           Control.Exception  (SomeException, try)
+import           Data.Maybe         (listToMaybe)
+import           System.Environment (getArgs, getProgName)
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  progName <- getProgName
+  case listToMaybe args of
+    Just sourceFileName -> do
+      sourceOrErr <- try (readFile sourceFileName) :: IO (Either SomeException String)
+      case sourceOrErr of
+        Right sourceCode ->
+          putStrLn sourceCode
+
+        Left readErr ->
+          putStrLn $ show readErr
+
+    Nothing ->
+      putStrLn $ "Usage: " ++ progName ++ " [Source File]"
